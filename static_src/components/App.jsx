ae574fb143450033f7dbbe8ee3b8c30035213a80
ParkOnProject <63898408+ParkOnProject@users.noreply.github.com>
2020-05-02 13:01:12 +0300

    Update App.jsx

--- static_src/components/App.jsx
+++ static_src/components/App.jsx
@@ -6,10 +6,13 @@ import style from "./styles/style.css";
 import {useMediaQuery} from 'react-responsive';
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Map from "./Map";
-import About from './About';
+import Welcome from './Welcome';
 import PhoneValidation from './PhoneValidation';
 import EmailValidation from './EmailValidation';
 import logo from "./img/logo.png";
+import Training from "./Training";
+import Letstraining from "./Letstraining";
+
 
 
 
@@ -25,8 +28,8 @@ render(){
 	 		<div className='App'>
 				<Switch>
 
-					<Route path="/About">
-						<About/>
+					<Route path="/Welcome">
+						<Welcome/>
 					</Route>
 					
 					<Route path="/Map">
@@ -40,10 +43,20 @@ render(){
 					<Route path="/EmailValidation">
 						<EmailValidation />
 					</Route>					
+
+					<Route path="/Training">
+						<Training/>
+					</Route>
+
+					<Route path="/Letstraining">
+						<Letstraining/>
+					</Route>		
 					
 					<Route path="/">
 						<MainScreen />
 					</Route>
+
+	
 					
 				</Switch>
 			</div>
